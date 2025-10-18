3e3de49c8df0892f9cb80a32737b9f8f7a55adb9
Koti Harshini <kharshinishetty2006@gmail.com>
2025-10-18 17:15:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,13 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMk7Kkol-v2PwVt_ySCFnXYVw9oCdRuB8",
-  authDomain: "appforge-24de0.firebaseapp.com",
-  projectId: "appforge-24de0",
-  storageBucket: "appforge-24de0.firebasestorage.app",
-  messagingSenderId: "753392130997",
-  appId: "1:753392130997:web:9809c7da239a2cf3dbd7fc",
-  measurementId: "G-11KM1J1D4C"
+  
 };
 
 // Initialize Firebase app
@@ -27,4 +21,5 @@ const auth = initializeAuth(app, {
 const db = getFirestore(app);
 
 // Export auth and db instances
+
 export { auth, db };
